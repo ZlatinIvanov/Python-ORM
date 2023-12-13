a3e1e1886e395609f5549eb9d8d73cd0b7f16d43
ZlatinIvanov <zlatin89@gmail.com>
2023-12-13 07:11:24 +0200

    Create caller.py

---
+++ 9. Exercise Data Operations in Django with Queries/Skeleton/caller.py
@@ -0,0 +1,146 @@
+import os
+import django
+from django.db.models import QuerySet
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+
+from main_app.models import Pet
+from main_app.models import Artifact
+from main_app.models import Location
+from main_app.models import Car
+from main_app.models import Task
+from main_app.models import HotelRoom
+
+
+def create_pet(name: str, species: str):
+    Pet.objects.create(
+        name=name,
+        species=species,
+    )
+    return f"{name} is a very cute {species}!"
+
+
+def create_artifact(name: str, origin: str, age: int, description: str, is_magical: bool):
+    Artifact.objects.create(
+        name=name,
+        origin=origin,
+        age=age,
+        description=description,
+        is_magical=is_magical,
+    )
+    return f"The artifact {name} is {age} years old!"
+
+
+def delete_all_artifacts():
+    Artifact.objects.all().delete()
+
+
+def show_all_locations():
+    locations = Location.objects.all().order_by('-id')
+    return '\n'.join(str(l) for l in locations)
+
+
+def new_capital():
+    # Location.objects.filter(pk=1).update(is_capital=True)
+    location = Location.objects.first()
+    location.is_capital = True
+    location.save()
+
+
+def get_capitals() -> QuerySet:
+    return Location.objects.filter(is_capital=True).values('name')
+
+
+def delete_first_location():
+    Location.objects.first().delete()
+
+
+def apply_discount() -> None:
+    cars = Car.objects.all()
+
+    for car in cars:
+        percentage_off = sum(int(x) for x in str(car.year)) / 100
+        discount = float(car.price) * percentage_off
+        car.price_with_discount = float(car.price) - discount
+        car.save()
+
+
+def get_recent_cars() -> QuerySet:
+    return Car.objects.filter(year__gte=2020).values('model', 'price_with_discount')
+
+
+def delete_last_car() -> None:
+    Car.objects.last().delete()
+
+
+def show_unfinished_tasks() -> str:
+    unfinished_tasks = Task.objects.filter(is_finished=False)
+
+    return '\n'.join(str(t) for t in unfinished_tasks)
+
+
+def complete_odd_tasks() -> None:
+    for task in Task.objects.all():
+        if task.id % 2 != 0:
+            task.is_finished = True
+            task.save()
+
+
+def encode_and_replace(text: str, task_title: str) -> None:
+    # decoded_text = ''.join(chr(ord(x) - 3) for x in text)
+    # Task.objects.filter(title=task_title).update(description=decoded_text)
+
+    tasks_with_matching_title = Task.objects.filter(title=task_title)
+    decoded_text = ''.join(chr(ord(x) - 3) for x in text)
+
+    for task in tasks_with_matching_title:
+        task.description = decoded_text
+        task.save()
+
+
+def get_deluxe_rooms() -> str:
+    deluxe_rooms = HotelRoom.objects.filter(room_type="Deluxe")
+    even_id_deluxe_rooms = []
+
+    for room in deluxe_rooms:
+        if room.id % 2 == 0:
+            even_id_deluxe_rooms.append(str(room))
+
+    return '\n'.join(even_id_deluxe_rooms)
+
+
+def increase_room_capacity() -> None:
+    rooms = HotelRoom.objects.all().order_by("id")
+
+    previous_room_capacity = None
+
+    for room in rooms:
+        if not room.is_reserved:
+            continue
+
+        if previous_room_capacity:
+            room.capacity += previous_room_capacity
+        else:
+            room.capacity += room.id
+
+        previous_room_capacity = room.capacity
+
+        room.save()
+
+
+def reserve_first_room() -> None:
+    first_room = HotelRoom.objects.first()
+    first_room.is_reserved = True
+    first_room.save()
+
+
+def delete_last_room() -> None:
+    last_room = HotelRoom.objects.last()
+
+    if last_room.is_reserved:
+        last_room.delete()
+
+

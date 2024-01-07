ae11132df19324c401ec0b5b88aa69d512dc28c7
ZlatinIvanov <zlatin89@gmail.com>
2024-01-07 11:04:05 +0200

    Create caller.py

---
+++ 19. Exercise Advanced Queries in Django/AdvancedQueries_Skeleton/caller.py
@@ -0,0 +1,14 @@
+import os
+import django
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+
+from main_app.models import RealEstateListing
+
+
+
+# Create and check models
+# Run and print your queries

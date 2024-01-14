fc59330cb36deb3bb9a0b65aee080d1808677d75
ZlatinIvanov <zlatin89@gmail.com>
2024-01-14 11:12:16 +0200

    Create custom_model_manager.py

---
+++ 22. Exam Preparation/Exam_Prep_Skeleton/main_app/custom_model_manager.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.db.models import Count
+
+
+class DirectorManager(models.Manager):
+
+    def get_directors_by_movies_count(self):
+        return self.annotate(num_movies=Count('director_movies')).order_by('-num_movies', 'full_name')
